77781235198c76f4857345c1360af3939a48d19e
julian <julianch36@gmail.com>
2023-09-28 15:15:29 +0700

    Edit Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/julian_201111382_devop/dist/",
+  base: "/julian_201111382_devop/",
   test: {
     globals: true,
     environment: "jsdom",
